59e5346632c40e6cd3885b2a2870c5172e680206
redelste <redelste@stevens.edu>
2020-09-02 14:07:12 -0700

    began adding basic funcitonality of side bar component. Setting up layout for routes and project components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
+import './index.css'
 import './App.css';
+import Asset1 from './assets/Asset3.svg'
+import Projects from './components/Projects'
+import { Grid } from '@material-ui/core';
+import Sidebar from './components/Sidebar'
 
+// What we need: 
+// About me
+// Projects
+// Contact ==> could just be a footer? 
+// ====================================
+// future development --> link to blog (a project probably)
 function App() {
+  //  color of the "ASSET": #f7f2e8
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="App-header" style={{ backgroundImage: `url(${Asset1})`, backgroundRepeat: "no-repeat" }}>
+        <Grid container spacing={1}>
+          {/* col */}
+          <Grid item xs={6} style={{ color: "black" }}>
+          </Grid>
+          {/* col 2 */}
+          <Grid container xs={6} style={{ color: "black", height: "100vh" }}>
+            <Sidebar></Sidebar>
+            <Grid container justify="center" alignItems="flex-start" xs={9} style={{ paddingTop: "10px" }}>
+              Ryan Edelstein
+            </Grid>
+            <Grid container style={{ marginTop: "-50px" }}>
+              <Grid item xs={4}>
+                About
+              </Grid>
+              <Grid item xs={4}>
+                Projects
+              </Grid>
+              <Grid item xs={4}>
+                Other
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
